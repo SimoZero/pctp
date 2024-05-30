f6fedb07c7e36b6ed0ce08cff890e14152f4fe68
Matteo Zerilli <Matteo Zerilli@LAPTOP-NLD7ATME>
2024-05-30 12:42:54 +0200

    Aggiungere i file di progetto.

---
+++ pctp/wwwroot/js/script.js
@@ -0,0 +1,9 @@
+﻿let copertine = ["https://m.media-amazon.com/images/I/412FZmYY0sL.jpg", "https://m.media-amazon.com/images/I/41dbYUr0rUL.jpg", "https://m.media-amazon.com/images/I/41R38uCEyGL.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfYGgvx44ivyiIuNymh8hWpBMpRBWI0HbrQg&s", "https://m.media-amazon.com/images/I/71yxc8fLNgL._AC_UF1000,1000_QL80_.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXpiuDCTdxqD3xwJgKUbl3NYk0tl3Xd3QEmA&s"];
+
+document.getElementById("bottoneCerca").addEventListener("click", function () {
+    let n1 = document.getElementById("intputRicerca").value;
+})
+
+document.getElementById("bottoneAvanti").addEventListener("click", function () {
+    document.getElementById("img1").
+})
